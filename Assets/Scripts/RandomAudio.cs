77a824e03be1026e1a173319ae8005cb1e649deb
heasp04 <heasp2004@gmail.com>
2024-01-28 14:22:38 +0100

    Made some random audio player

---
+++ Assets/Scripts/RandomAudio.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomAudio : MonoBehaviour
+{
+    public AudioClip[] audioClips;
+    //public AudioSource audioSource;
+    
+    public float minTime = 15f;
+    public float maxTime = 20f;
+
+    private float timer = 30f;
+    
+    
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            PlayAudio();
+            timer = Random.Range(minTime, maxTime);
+        }
+        
+    }
+
+    private void PlayAudio()
+    {
+        int index = Random.Range(0, audioClips.Length);
+        
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.clip = audioClips[index];
+        audio.Play();
+    }
+}
